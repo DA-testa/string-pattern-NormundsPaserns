3c93521e5908ffeddb48b5d9c672f837dc5e6f45
Normunds Paserns <124579224+NormundsPaserns@users.noreply.github.com>
2023-04-15 11:14:59 +0000

    commit5

--- hash_substring.py
+++ hash_substring.py
@@ -10,8 +10,7 @@ def read_input():
         pattern = input("Enter pattern:").rstrip()
         text = input("Enter text:").rstrip()
     elif input_type.upper() == 'F':
-        #read from file
-        with open("/workspaces/string-pattern-NormundsPaserns/tests/06" 'r') as f:
+        with open("tests/06" 'r') as f:
             pattern = f.readline().rstrip()
             text = f.readline().rstrip()
     else:
